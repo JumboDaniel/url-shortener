b49a25a35f204abd51b8c623b06fd26146f5d662
Jumbo Daniel <jumbodaniel15@gmail.com>
2021-07-09 01:31:21 +0800

    Linked the schema to the server , setup url routes, and add clicks configuration

--- model/shorturls.js
+++ model/shorturls.js
@@ -1,16 +1,24 @@
-const { Mongoose } = require("mongoose");
-const schenma = Mongoose.schema;
 
-const shortUrlsSchema = new schenma({
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const shortid = require("shortid");
+
+//schema
+const shortUrlsSchema = new Schema({
     full:{
         type: String,
         required: true
     },
     short:{
         type: String,
-        required: true
+        required: true,
+        default: shortid.generate
     },
     clicks:{
-
+        type: Number,
+        required: true,
+        default: 0
     }
 })
+
+module.exports = mongoose.model('shorturl', shortUrlsSchema)
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 //express connect 
 const express = require('express')
 const app= express();
+const ShortUrl = require('./model/shorturls')
 
 //mongoose connection
 const mongoose = require('mongoose');
-// const dbURI = 'mongodb://localhost/urlshortner';
-mongoose.connect('mongodb://localhost/urlshortner', {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
+const dbURI = 'mongodb://localhost/urlshortner';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result)=>console.log('connected')) 
+.catch((err) =>console.log(err));
+// mongoose.connect('mongodb://localhost/urlshortner', {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// })
 
 //viewengine
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
 
 //routes
-app.get('/', (req, res)=>{
-    res.render('index')
+app.get('/', async (req, res)=>{
+    const shorturls = await ShortUrl.find()
+    res.render('index', {shorturl: shorturls})
 })
 
-app.post('/shorturls', (req, res)=>{
+app.post('/shorturls', async (req, res)=>{
+    await ShortUrl.create({full: req.body.fullUrl })
+    res.redirect('/')
+})
 
+app.get('/:shorturls', async (req, res)=>{
+    const shorturls = await ShortUrl.findOne({short: req.params.shorturls})
+    
+    if(shorturls == null) return res.sendStatus(404)
+    
+    shorturls.clicks++
+    shorturls.save()
+    res.redirect(shorturls.full)
 })
 
 //ports 
